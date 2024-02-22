ff9d91024bc2d49b0bfed676caed6c97e4ed9d62
AllUserNamesAreTakenHere <rosa8454bahadori@gmail.com>
2024-02-22 23:02:01 +0330

    started the work with building colleges .

---
+++ src/CollegeOfMathematicsScience.java
@@ -0,0 +1,8 @@
+public class CollegeOfMathematicsScience extends Colleges{
+    protected CollegeOfMathematicsScience(int collegeCode, String collegeName) {
+        super(collegeCode, collegeName);
+        collegeCode=20;
+        collegeName="MathematicsScience";
+    }
+
+}
